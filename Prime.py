6577f31fcc970e00cbe23fb9bb014907da3e0960
sabbasza <Shiva@dn0a22813b.sunet>
2014-08-05 10:45:02 -0700

    Prime.py

---
+++ Prime.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Aug  4 11:58:03 2014
+
+@author: Shiva
+"""
+
+minIdx=1;
+maxIdx=1001;
+for i in range(minIdx,maxIdx):
+    isPrime=True;
+    for j in range(2,i):
+        if (i%j==0):
+            isPrime=False;
+            break            
+    if(isPrime==True):
+            print i
